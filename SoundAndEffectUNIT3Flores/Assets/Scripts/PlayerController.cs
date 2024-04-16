2e58854491c5988a917476c37abc3f679c905ac2
Jorge Flores <720624@student.dusd.net>
2024-04-16 11:42:02 -0700

    added new scripts
    added new scripts to the character and animation

--- SoundAndEffectUNIT3Flores/Assets/Scripts/PlayerController.cs
+++ SoundAndEffectUNIT3Flores/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +23,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnim = GetComponent<Animator>();
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+
+
+        if (collision.gameObject.CompareTag("Ground")) 
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle")) 
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+        }
     }
 }
